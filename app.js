20754e7409175b3dc79244ac5d89ab3934440a2b
ARod <ajr.rodriguez095@gmail.com>
2022-02-22 21:29:19 -0500

    added inquirer and prompt first question

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-// references page template js file to use code in it
-const generatePage = require("./src/page-template");
 
-// capture user input
-const profileDataArgs = process.argv.slice(2);
-// stores input data in variables
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
 
-//const pageHTML = generatePage(name, github);
+ // references page template js file to use code in it
+// const generatePage = require("./src/page-template");
 
-// creates html file
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// const pageHTML = generatePage(name,github);
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// // creates html file
+// fs.writeFile("index.html", pageHTML, err => {
+//     if(err) throw err;
 
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+inquirer
+  .prompt([
+      {
+          type: "input",
+          name: "name",
+          message: "What is your name?"
+      }
+  ])
+    .then(answers => console.log(answers));
